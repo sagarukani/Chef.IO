88ba9ddb6aac746109d7f89af406c7fd0b162564
Bhargav Chauhan <b.chauhan1806@gmail.com>
2023-10-11 15:18:06 -0400

    Backend files updated

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+app.post("/register", async (req, res) => {
+    try{
+        
+    }
+
+});
